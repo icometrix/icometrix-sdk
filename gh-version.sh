87db34acf42bdfd80145849e33d5a104975f78e1
Jeroen Pinxten <jeroen.pinxten@icometrix.com>
2024-02-02 16:56:43 +0100

    dynamic GH version

--- gh-version.sh
+++ gh-version.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # from refs/tags/v1.2.3 get 1.2.3
-VERSION=$(echo $GITHUB_REF | sed 's#.*/v##')
+VERSION=$(echo $GITHUB_REF | sed -E 's/refs\/tags\/v?([0-9]+\.[0-9]+\.[0-9]+)/\1/')
 PLACEHOLDER='__version__ = "0.0.dev0"'
 VERSION_FILE='icometrix_sdk/_version.py'
 
 # ensure the placeholder is there. If grep doesn't find the placeholder
 # it exits with exit code 1 and github actions aborts the build.
 grep "$PLACEHOLDER" "$VERSION_FILE"
-sed -i "s/$PLACEHOLDER/__version__ = \"${VERSION}\"/g" "$VERSION_FILE"
+sed -i '' "s/$PLACEHOLDER/__version__ = \"${VERSION}\"/g" "$VERSION_FILE"
