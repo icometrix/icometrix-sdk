c951c2cdfb7c39007e0560722e449b90f4f3433c
Jeroen Pinxten <jeroen.pinxten@icometrix.com>
2024-02-02 16:31:27 +0100

    valid DEV semver

--- gh-version.sh
+++ gh-version.sh
@@ -2,7 +2,7 @@
 
 # from refs/tags/v1.2.3 get 1.2.3
 VERSION=$(echo $GITHUB_REF | sed 's#.*/v##')
-PLACEHOLDER='__version__ = "develop"'
+PLACEHOLDER='__version__ = "0.0.dev0"'
 VERSION_FILE='icometrix_sdk/_version.py'
 
 # ensure the placeholder is there. If grep doesn't find the placeholder
